5ca0cb2a319523344f936e37c0947f0707eaaec5
Martin Heni <martin@heni-online.de>
2000-11-08 21:27:05 +0000

    Yet another initial import of the libkdegames (kcardialog)
    svn path=/trunk/kdegames/libkdegames/; revision=70344

---
+++ kcarddialog.cpp
@@ -0,0 +1,425 @@
+/***************************************************************************
+                          KCardDialog  -  Change player names
+                             -------------------
+    begin                : 31 Oct 2000
+    copyright            : (C) 2000 by Martin Heni
+    email                : martin@heni-online.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/*
+    $Id$
+*/
+
+/* TODO:
+ *  - Check for tailing "/" in getDefaultXXX
+ *  - check whether alternate dirs exists!
+ *
+ */
+
+#include <qgroupbox.h>
+#include <qpushbutton.h>
+#include <qlabel.h>
+#include <qdir.h>
+#include <stdio.h>
+#include <klocale.h>
+#include <kstddirs.h>
+#include "kcarddialog.h"
+
+int KCardDialog::getCardDeck(QString &mDeck,QString &mCarddir,QWidget *mParent,int mFlags,
+                       QString mAlternateDeck,QString mAlternateCarddir,QString mMask)
+{
+
+
+  KCardDialog dlg(mParent);
+  int result;
+
+  // Default filemaks is a PNG icon
+  if (mMask.isNull()) mMask=QString::fromLatin1(".png");
+  // Remove mask from deck if given
+  if (!mDeck.isNull() && !mMask.isNull())
+  {
+    if (mDeck.right(mMask.length())==mMask) mDeck=mDeck.left(mDeck.length()-mMask.length());
+  }
+
+  dlg.setDeck(mDeck);
+  dlg.setCarddir(mCarddir);
+  dlg.setFlags(mFlags);
+  dlg.setFilemask(mMask);
+  dlg.setAlternateDeck(mAlternateDeck);
+  dlg.setAlternateCarddir(mAlternateCarddir);
+  
+  dlg.setupDialog();
+  result=dlg.exec();
+  if (result==QDialog::Accepted)
+  {
+    mDeck=dlg.deck();
+    if (!mDeck.isNull())
+    {
+      mDeck+=dlg.filemask();
+    }
+    mCarddir=dlg.carddir();
+    if (!mCarddir.isNull() && mCarddir.right(1)!=QString::fromLatin1("/"))
+    {
+      mCarddir+=QString::fromLatin1("/");
+    }
+  }
+  return result;
+}
+
+QString KCardDialog::getDefaultDeckpath(int mFlags,QString mAlternateDeck)
+{
+    QString file=KCARD_DECKPATH+KCARD_DEFAULTDECK+KCARD_DEFAULTFILEMASK;
+    QString path=kapp->dirs()->findResourceDir("data",file);
+    if (!path.isNull())
+    {
+      path+=KCARD_DECKPATH;
+    }
+
+    if (mAlternateDeck.isNull() || (!path.isNull() && (mFlags&ProbeDefaultDir)))
+    {
+      return path;
+    }
+    else
+    {
+      return mAlternateDeck;
+    }
+}
+
+QString KCardDialog::getDefaultCardpath(int mFlags,QString mAlternateCarddir)
+{
+    QString file=KCARD_CARDPATH+KCARD_DEFAULTCARDDIR+KCARD_DEFAULTCARD+KCARD_DEFAULTFILEMASK;
+    QString path=kapp->dirs()->findResourceDir("data",file);
+    if (!path.isNull())
+    {
+      path+=KCARD_CARDPATH+KCARD_DEFAULTCARDDIR;
+    }
+
+    if (mAlternateCarddir.isNull() || (!path.isNull() && (mFlags&ProbeDefaultDir)))
+    {
+      return path;
+    }
+    else
+    {
+      return mAlternateCarddir;
+    }
+}
+
+QString KCardDialog::deck() { return cDeck; }
+void KCardDialog::setDeck(QString file) { cDeck=file; }
+QString KCardDialog::carddir() { return cCardDir; }
+void KCardDialog::setCarddir(QString dir) { cCardDir=dir; }
+int KCardDialog::flags() { return cFlags; }
+void KCardDialog::setFlags(int f) { cFlags=f; }
+QString KCardDialog::filemask() { return cMask; }
+void KCardDialog::setFilemask(QString mask) { cMask=mask; }
+QString KCardDialog::alternateDeck() { return cAltDeck; }
+void KCardDialog::setAlternateDeck(QString file) { cAltDeck=file; }
+QString KCardDialog::alternateCarddir() { return cAltCarddir; }
+void KCardDialog::setAlternateCarddir(QString dir) { cAltCarddir=dir; }
+
+// protected
+QString KCardDialog::deckpath() { return cDeckpath; }
+void KCardDialog::setDeckpath(QString path) { cDeckpath=path; }
+QString KCardDialog::cardpath() { return cCardpath; }
+void KCardDialog::setCardpath(QString path) { cCardpath=path; }
+
+void KCardDialog::setupDialog()
+{
+  QString file,path;
+  int y_pos,y_pos2;
+  QWMatrix m;
+  m.scale(0.8,0.8);
+
+  setCaption(i18n("Carddeck selection"));
+  setMinimumSize(200,160);
+  setMaximumSize(800,600);                                                        
+
+  if (flags() & (NoDeck|NoCards))
+  {
+    resize(550,240);
+    y_pos=200;
+    y_pos2=0;
+  }
+  else
+  {
+    resize( 550, 480 );
+    y_pos=440;
+    y_pos2=220;
+  }
+
+  if (! (flags() & NoDeck))
+  {
+    // Deck iconview
+    QGroupBox* grp1;
+    grp1 = new QGroupBox(i18n("Choose backside"), this);
+    grp1->resize(380,200);
+    grp1->move(10,10);
+
+    deckIconView =new KIconView(grp1,"decks");
+    deckIconView->setGeometry( 10, 20, 360, 170 );
+    deckIconView->setGridX(36);
+    deckIconView->setGridY(50);
+    deckIconView->setSelectionMode(QIconView::Single);
+
+    // deck select
+    QGroupBox* grp3;
+    grp3 = new QGroupBox(i18n("Back"), this);
+    grp3->resize(100,130);
+    grp3->move(400,10);
+
+    deckLabel = new QLabel(grp3);
+    deckLabel->setGeometry(10, 20, 80, 90 );
+    deckLabel->setText(i18n("empty"));
+    deckLabel->setAlignment(AlignHCenter|AlignVCenter);
+
+    connect(deckIconView,SIGNAL(clicked(QIconViewItem *)),
+            this,SLOT(slotDeckClicked(QIconViewItem *)));
+  }
+
+  if (! (flags() & NoCards))
+  {
+    // Cards iconview
+    QGroupBox* grp2;
+    grp2 = new QGroupBox(i18n("Choose frontside"), this);
+    grp2->resize(380,200);
+    grp2->move(10,10+y_pos2);
+
+    cardIconView =new KIconView(grp2,"cards");
+    cardIconView->setGeometry( 10, 20, 360, 170 );
+    cardIconView->setGridX(36);
+    cardIconView->setGridY(50);
+
+    // Card select
+    QGroupBox* grp4;
+    grp4 = new QGroupBox(i18n("Front"), this);
+    grp4->resize(100,130);
+    grp4->move(400,10+y_pos2);
+
+    cardLabel = new QLabel(grp4);
+    cardLabel->setGeometry(10, 20, 80, 90 );
+    cardLabel->setText(i18n("empty"));
+    cardLabel->setAlignment(AlignHCenter|AlignVCenter);
+
+    connect(cardIconView,SIGNAL(clicked(QIconViewItem *)),
+            this,SLOT(slotCardClicked(QIconViewItem *)));
+  }
+
+
+
+  // Buttons
+  QPushButton *PushButton;
+  PushButton = new QPushButton( this, "Button_1" );
+  PushButton->setGeometry( 390, y_pos, 65, 30 );
+  connect( PushButton, SIGNAL(clicked()), SLOT(accept()) );
+  PushButton->setText( i18n("OK" ));
+  PushButton->setAutoRepeat( FALSE );
+  PushButton->setAutoResize( FALSE );
+
+  PushButton = new QPushButton( this, "Button_2" );
+  PushButton->setGeometry( 470, y_pos, 65, 30 );
+  connect( PushButton, SIGNAL(clicked()), SLOT(reject()) );
+  PushButton->setText( i18n("Cancel" ));
+  PushButton->setAutoRepeat( FALSE );
+  PushButton->setAutoResize( FALSE );
+
+
+  // Insert Deckicons 
+  // First find the default or alternate path
+ 
+  if (! (flags() & NoDeck))
+  {
+    file=KCARD_DECKPATH+KCARD_DEFAULTDECK+filemask();
+    path=kapp->dirs()->findResourceDir("data",file);
+    if (!path.isNull())
+    {
+      path+=KCARD_DECKPATH;
+    }
+
+
+    // Set default pat if no alternate deck given or probe default deck given
+    // and default deck found
+    if (alternateDeck().isNull() || (!path.isNull() && (flags()&ProbeDefaultDir)))
+    {
+      setDeckpath(path);
+    }
+    else
+    {
+      setDeckpath(alternateDeck());
+    }
+
+
+    InsertDeckIcons(deckpath());
+    deckIconView->arrangeItemsInGrid();
+ 
+    // Set default icons if given
+    if (!deck().isNull())
+    {
+      file=deck()+filemask();
+      QPixmap *pixmap=new QPixmap(file);
+      QPixmap *pixmap2=new QPixmap;
+      *pixmap2=pixmap->xForm(m);
+      delete pixmap;
+      deckLabel->setPixmap(*pixmap2);
+      pixmapList.append(pixmap2);
+    }
+  }
+
+  // Insert cardicons
+  if (! (flags() & NoCards))
+  {
+    file=KCARD_CARDPATH+KCARD_DEFAULTCARDDIR+KCARD_DEFAULTCARD+filemask();
+    path=kapp->dirs()->findResourceDir("data",file);
+    if (!path.isNull())
+    {
+      path+=KCARD_CARDPATH;
+    }
+    // Set default pat if no alternate deck given or probe default deck given
+    // and default deck found
+    if (alternateCarddir().isNull() || (!path.isNull() && (flags()&ProbeDefaultDir)))
+    {
+      setCardpath(path);
+    }
+    else
+    {
+      setCardpath(alternateCarddir());
+    }
+
+    InsertCardIcons(cardpath());
+    cardIconView->arrangeItemsInGrid();
+
+    // Set default icons if given
+    if (!carddir().isNull())
+    {
+      file=carddir()+KCARD_DEFAULTCARD+filemask();
+      QPixmap *pixmap=new QPixmap(file);
+      QPixmap *pixmap2=new QPixmap;
+      *pixmap2=pixmap->xForm(m);
+      delete pixmap;
+      cardLabel->setPixmap(*pixmap2);
+      pixmapList.append(pixmap2);
+    }
+  }
+
+}
+
+void KCardDialog::InsertCardIcons(QString path)
+{
+  QDir dir;
+  QString label;
+  unsigned int i;
+  // We shrink the icons a little
+  QWMatrix m;
+  m.scale(0.8,0.8);
+
+  dir.cd(path);
+  dir.setSorting(QDir::Name);
+  dir.setNameFilter("card*");
+  
+  for (i=0;i<dir.count();i++)
+  {
+    QString card=QString::fromLatin1("/11")+filemask();
+    label=dir[i]+card;
+
+  
+    QPixmap *pixmap=new QPixmap(path+label);
+    if (pixmap->isNull())
+    {
+      delete pixmap;
+      continue;
+    }
+   
+    QPixmap *pixmap2=new QPixmap;
+    *pixmap2=pixmap->xForm(m);
+    delete pixmap;
+
+    pixmapList.append(pixmap2);
+  
+    QIconViewItem *item= new QIconViewItem(cardIconView,dir[i],*pixmap2);
+    item->setDragEnabled(false);
+    item->setDropEnabled(false);
+    item->setRenameEnabled(false);
+    item->setSelectable(true);
+    
+  }
+}
+  
+void KCardDialog::InsertDeckIcons(QString path)
+{
+  QDir dir;
+  QString label;
+  unsigned int i;
+  
+  // We shrink the icons a little
+  QWMatrix m;
+  m.scale(0.8,0.8);
+
+  // Read in directory sorted by name
+  dir.cd(path);
+  dir.setSorting(QDir::Name);
+  dir.setNameFilter(QString::fromLatin1("*")+filemask());
+  
+  for (i=0;i<dir.count();i++)
+  {
+    label=dir[i];
+    QPixmap *pixmap=new QPixmap(path+label);
+    if (pixmap->isNull())
+    {
+      delete pixmap;
+      continue;
+    }
+    // Transform bitmap (shrink)
+    QPixmap *pixmap2=new QPixmap;
+    *pixmap2=pixmap->xForm(m);
+    delete pixmap;
+
+    // To get rid of the pixmaps later on
+    pixmapList.append(pixmap2);
+  
+    QIconViewItem *item= new QIconViewItem(deckIconView,label.left(label.length()-filemask().length()),
+                                           *pixmap2);
+    item->setDragEnabled(false);
+    item->setDropEnabled(false);
+    item->setRenameEnabled(false);
+  }
+}
+
+
+KCardDialog::~KCardDialog()
+{
+}
+
+
+// Create the dialog 
+KCardDialog::KCardDialog( QWidget *parent, const char *name,bool modal , WFlags f )
+    : KDialog( parent, name,modal,f )
+{
+  pixmapList.setAutoDelete(TRUE);
+}
+
+void KCardDialog::slotDeckClicked(QIconViewItem *item)
+{
+    if (item && item->pixmap()) 
+    {
+      deckLabel->setPixmap(* (item->pixmap()));
+      setDeck(deckpath()+item->text());
+    }
+}
+void KCardDialog::slotCardClicked(QIconViewItem *item)
+{
+    if (item && item->pixmap()) 
+    {
+      cardLabel->setPixmap(* (item->pixmap()));
+      setCarddir(cardpath()+item->text());
+    }
+
+}
+
+#include "kcarddialog.moc"
---
+++ kcarddialog.h
@@ -0,0 +1,296 @@
+/***************************************************************************
+                           KCardDialog
+                           -------------------
+    begin                : 30 October 2000 
+    copyright            : (C) 2000 by Martin Heni
+    email                : martin@heni-online.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef __KCARDDLG_H_
+#define __KCARDDLG_H_
+#include <kapp.h>
+#include <qstring.h>
+#include <qdialog.h>
+#include <qlabel.h>
+#include <kiconview.h>
+#include <kdialog.h>
+
+#define KCARD_DECKPATH QString::fromLatin1("carddecks/decks/")
+#define KCARD_DEFAULTDECK QString::fromLatin1("deck0")
+#define KCARD_CARDPATH QString::fromLatin1("carddecks/")
+#define KCARD_DEFAULTCARD QString::fromLatin1("11")
+#define KCARD_DEFAULTCARDDIR QString::fromLatin1("cards1/")
+#define KCARD_DEFAULTFILEMASK QString::fromLatin1(".png");
+
+
+/**
+ * The KCardDialog provides a dialog for interactive carddeck selection.
+ * It gives cardgames an easy to use interface to select front and
+ * back of the card sets. As card sets the KDE default cardsets are
+ * offered as well as used specified ones.
+ *
+ * In most cases, the simplest
+ * use of this class is the static method @ref KCardDialog::getCardDeck(),
+ * which pops up the dialog, allows the user to select a carddeck, and
+ * returns when the dialog is closed. Only if you really need some specific
+ * behaviour or if you overwrite the dialog you need all the other access
+ * functions.
+ *
+ * Example:
+ *
+ * <pre>
+ *      QString deck,card;
+ *      int result = KCardDialog::getCardDeck(deck,card );
+ *      if ( result == KCardDialog::Accepted )
+ *            ...
+ * </pre>
+ *
+ * Here you can see a card dialog in action
+ * @image kcarddialog.png KCarddialog
+ *
+ * @short A carddeck selection dialog for card games.
+ * @author Martin Heni <martin@heni-online.de>
+ * @version $Id:$
+ */
+class KCardDialog : public KDialog
+{
+  Q_OBJECT
+
+public:
+   /**
+   * Constructs a card deck selection dialog.
+   *
+   * @param parent The parent widget of the dialog, if any.
+   * @param name The name of the dialog.
+   * @param modal Specifies whether the dialog is modal or not.
+   * @param f Passes window flags to the dialog.
+   *
+   */
+   KCardDialog (QWidget* parent = NULL,const char* name = NULL,
+             bool modal = true, WFlags f = 0);
+   /**
+   * Destructs a card deck selection dialog.
+   *
+   */
+   ~KCardDialog();
+
+   /**
+   *  @li @p NoDeck - The deck (back) selection is not shown
+   *  @li @p NoCards -  The cards (front) selection is not shown
+   *  @li @p ProbeDefaultDir - Always probes for the KDE default carddir
+   */
+   enum CardMask { NoDeck=0x01, NoCards=0x02, ProbeDefaultDir=0x04 };
+
+   /**
+   * Creates a modal carddeck dialog, lets the user choose a deck,
+   * and returns when the dialog is closed.
+   *
+   * @param mDeck a reference to the filename used as backside of the
+   *        cards. It is an absolute path and can directly be loaded as
+   *        pixmap.
+   *
+   * @param mCarddir a reference to the directory name used as front of the
+   *        cards. The directory contains the card images as 1.png to 52.png
+   *
+   * @param mParent an optional pointer to the parent window of the dialog
+   *
+   * @param mFlags An or'ed combination of @p NoDeck, @p NoCards and
+   *               @p ProbeDefaultDir 
+   *
+   * @param mAlternateDeck An optinal path to the directory where the
+   *        card decks are stored. If @p ProbeDefaultDir is given and
+   *        the default directory is found the default directory is
+   *        taken otherwise the alternative one
+   *
+   * @param  mAlternateCarddir An optinoal path to the directory where
+   *        the carddecks are stored (in subdirectories cards1,cards2,...).
+   *        It behaves as @p mAlternateDeck otherwise
+   *
+   * @param mMask An optinonal filemask for the icons. Per default *.png
+   *        images are assumed.
+   *
+   * @return @ref #QDialog::result().
+   */
+   static int getCardDeck(QString &mDeck,QString &mCarddir,QWidget *mParent=0,
+                       int mFlags=0, QString mAlternateDeck=0,
+                       QString mAlternateCarddir=0,QString mMask=0);
+
+   /**
+   * Returns the default path to the card deck backsides. You want
+   * to use this usually before the user used the card dialog the first
+   * time to get an default deck. You can assume that
+   * <pre>
+   *   getDefaultDeckpath()+QString::fromLatin1("deck0.png")
+   * </pre>
+   * is a valid deck.
+   *
+   * @return The default path
+   *
+   * @param Optional
+   *        you can give the flag @p ProbeDefaultDir and an
+   *        alternative deckpath. It works as in @ref #getCardDeck, i.e.
+   *        the decks are taken from the alternative path if given. If
+   *        the probing option is given the default directory is preferred
+   *        if existant
+   */
+   static QString getDefaultDeckpath(int mFlags=0,QString mAlternateDeck=0 );
+
+   /**
+   * Returns the default path to the card frontsides. You want
+   * to use this usually before the user used the card dialog the first
+   * time to get an default deck. You can assume that
+   * <pre>
+   *   getDefaultCardpath()+QString::fromLatin1("*.png")
+   * </pre>
+   * are valid cards for * from 1 to 52.
+   *
+   * @param Optionial you can give the flag @p ProbeDefaultDir and an
+   *        alternative deckpath. It works as in @ref #getCardDeck, i.e.
+   *        the decks are taken from the alternative path if given. If
+   *        the probing option is given the default directory is preferred
+   *        if existant
+   *
+   * @return returns the path to the card directory
+   */
+   static QString getDefaultCardpath(int mFlags=0,QString mAlternateCarddir=0);
+
+
+   /**
+   * Returns the choosen deck, which is a valid path to a imagefile.
+   * 
+   * @return The deck
+   */
+   QString deck();
+
+   /**
+   * Sets the default deck.
+   *
+   * @param file The full path to an image file
+   * 
+   */
+   void setDeck(QString file);
+
+   /**
+   * Returns the choosen card directory
+   * 
+   * @return The card directory
+   */
+   QString carddir();
+
+   /**
+   * Sets the default card directory.
+   *
+   * @param dir The full path to an card directory
+   * 
+   */
+   void setCarddir(QString dir);
+
+   /**
+   * Returns the flags set to the dialog
+   * 
+   * @return the flags
+   */
+   int flags();
+
+   /**
+   * Sets the flags for the dialog
+   *
+   * @param f The flags @p NoDeck, @p NoCards and @p ProbeDefaultDir 
+   * 
+   */
+   void setFlags(int f);
+
+   /**
+   * Returns the filemask
+   * 
+   * @return the filemask
+   */
+   QString filemask();
+
+   /**
+   * Sets the filemask
+   *
+   * @param mask The filemask (default ".png")
+   * 
+   */
+   void setFilemask(QString mask);
+
+   /**
+   * Returns the alternative deck directory
+   *
+   * @return the directory
+   * 
+   */
+   QString alternateDeck();
+
+   /**
+   * Sets the alternative deck directory
+   *
+   * @param file The directory where the decks (deck0.png, deck1.png,...)
+   *       are stored
+   * 
+   * @return the alternative deck directory
+   */
+   void setAlternateDeck(QString file);
+
+   /**
+   * Returns the alternative card directory
+   * 
+   * @return the alternative card directory
+   */
+   QString alternateCarddir();
+
+   /**
+   * Sets the alternative card directory
+   *
+   * @param dir The directory where the card subdirectories as
+   *       cards1, cards2, ... are stored
+   * 
+   */
+   void setAlternateCarddir(QString dir);
+
+   /**
+   * Creates the default widgets in the dialog. Must be called after
+   * all flags are set. This is only needed if you do NOT use the
+   * @ref #getCardDeck static function which provides all calls for you.
+   * 
+   */
+   void setupDialog();
+
+protected:
+
+protected slots:
+   void slotDeckClicked(QIconViewItem *);
+   void slotCardClicked(QIconViewItem *);
+
+private:
+   QString deckpath();
+   void setDeckpath(QString path);
+   QString cardpath();
+   void setCardpath(QString path);
+   void InsertCardIcons(QString path);
+   void InsertDeckIcons(QString path);
+
+
+private:
+   QList <QPixmap > pixmapList;
+   QLabel *deckLabel;
+   QLabel *cardLabel;
+   KIconView *deckIconView;
+   KIconView *cardIconView;
+
+   // set/query variables
+   QString cDeck,cCardDir,cMask;
+   QString cAltDeck,cAltCarddir;
+   int cFlags;
+   QString cDeckpath,cCardpath;
+};
+#endif
