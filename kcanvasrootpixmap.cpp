32fdf9416eeba2df9a409b832aadb31952fddf14
Nicolas Hadacek <hadacek@kde.org>
2002-04-23 18:27:33 +0000

    adding KCanvasRootPixmap class which is an implementation of KRootPixmap for QCanvasView. Currently used in ksirtet, very lightweight and can be useful for other developers imho.
    svn path=/trunk/kdegames/libkdegames/; revision=151497

---
+++ kcanvasrootpixmap.cpp
@@ -0,0 +1,20 @@
+#include "kcanvasrootpixmap.h"
+
+#include <qcanvas.h>
+
+
+KCanvasRootPixmap::KCanvasRootPixmap(QCanvasView *view, const char *name)
+    : KRootPixmap(view, name), _view(view)
+{
+    setCustomPainting(true);
+    connect(this, SIGNAL(backgroundUpdated(const QPixmap &)),
+            SLOT(backgroundUpdatedSlot(const QPixmap &)));
+}
+
+void KCanvasRootPixmap::backgroundUpdatedSlot(const QPixmap &pixmap)
+{
+    if ( _view && _view->canvas() )
+        _view->canvas()->setBackgroundPixmap(pixmap);
+}
+
+#include "kcanvasrootpixmap.moc"
