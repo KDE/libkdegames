9c56e581e0accd49f73a43a94f57baf4c0ba69c1
Mauricio Piacentini <piacentini@kde.org>
2007-04-17 02:51:44 +0000

    New KGameClock class, a generic game timer that can be paused and restarted. Returns the play time as a string. Does not provide presentation, only the clock. This is now implemented in KMines and  Will replace similar classes in KMahjongg, KShisen and possibly other games.
    svn path=/trunk/KDE/kdegames/libkdegames/; revision=654822

---
+++ kgameclock.cpp
@@ -0,0 +1,127 @@
+/*
+    This file is part of the KDE games library
+    Copyright (C) 2007 Mauricio Piacentini (mauricio@tabuleiro.com)
+    Portions reused from KGameLCDClock
+    Copyright (C) 2001,2002,2003 Nicolas Hadacek (hadacek@kde.org)
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#include "kgameclock.h"
+#include "kgameclock.moc"
+
+#include <QTimer>
+#include <QDebug>
+
+class KGameClockPrivate
+{
+public:
+    QTimer *timerClock;
+    uint    sec, min, hour;
+    KGameClock::ClockType clocktype;
+};
+
+KGameClock::KGameClock(QObject *parent, KGameClock::ClockType clocktype)
+: QObject(parent) , d(new KGameClockPrivate)
+{
+    d->clocktype = clocktype;
+    d->timerClock = new QTimer(this);
+    connect(d->timerClock, SIGNAL(timeout()), SLOT(timeoutClock()));
+}
+
+KGameClock::~KGameClock()
+{}
+
+void KGameClock::timeoutClock()
+{
+    if ( d->hour==23 && d->min==59 && d->sec==59 ) return;
+    d->sec++;
+    if (d->sec==60) {
+        d->min++;
+        d->sec = 0;
+    }
+    if (d->min==60) {
+        d->hour++;
+        d->min = 0;
+    }
+    showTime();
+}
+
+QString KGameClock::minSecString() const
+{
+    QString sec = QString::number(d->sec).rightJustified(2, '0', true);
+    QString min = QString::number(d->min).rightJustified(2, '0', true);
+    return min + ':' + sec;
+}
+
+QString KGameClock::hourMinSecString() const
+{
+    QString sec = QString::number(d->sec).rightJustified(2, '0', true);
+    QString min = QString::number(d->min).rightJustified(2, '0', true);
+    QString hour = QString::number(d->hour).rightJustified(2, '0', true);
+    return hour + ':' + min + ':' + sec;
+}
+
+void KGameClock::showTime()
+{
+    if (d->clocktype==MinSecOnly)
+      emit timeChanged(minSecString());
+    else
+      emit timeChanged(hourMinSecString());
+
+qDebug() << "showtime called" << hourMinSecString();
+}
+
+void KGameClock::restart()
+{
+    d->timerClock->stop();
+    d->sec = 0;
+    d->min = 0;
+    d->hour = 0;
+    showTime();
+}
+
+void KGameClock::resume()
+{
+    d->timerClock->start(1000); // 1 second
+}
+
+void KGameClock::pause()
+{
+    d->timerClock->stop();
+}
+
+uint KGameClock::seconds() const
+{
+    return d->hour*3600 + d->min*60 + d->sec;
+}
+
+void KGameClock::setTime(uint sec)
+{
+    Q_ASSERT( sec<(3600*24) );
+    d->sec = sec % 60;
+    d->min = (sec / 60) % 60;
+    d->hour = sec / 1440 ;
+    showTime();
+}
+
+void KGameClock::setTime(const QString &s)
+{
+    Q_ASSERT( s.length()==8 && s[2]==':' && s[5]==':' );
+    uint hour = qMin(s.section(':', 0, 0).toUInt(), uint(23));
+    uint min = qMin(s.section(':', 1, 1).toUInt(), uint(59));
+    uint sec = qMin(s.section(':', 2, 2).toUInt(), uint(59));
+    setTime(sec + min*60 + hour*3600);
+}
---
+++ kgameclock.h
@@ -0,0 +1,106 @@
+/*
+    This file is part of the KDE games library
+    Copyright (C) 2007 Mauricio Piacentini (mauricio@tabuleiro.com)
+    Portions reused from KGameLCDClock
+    Copyright (C) 2001,2002,2003 Nicolas Hadacek (hadacek@kde.org)
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#ifndef __KGAMELCD_H
+#define __KGAMELCD_H
+
+#include <QObject>
+#include <libkdegames_export.h>
+
+class QTimer;
+class KGameClockPrivate;
+
+/**
+ * Class representing a game clock, wraps after 24 hours
+ */
+class KDEGAMES_EXPORT KGameClock : public QObject
+{
+    Q_OBJECT
+public:
+    enum ClockType { HourMinSec = 0, MinSecOnly };
+
+    /**
+     * @return Constructor
+     */
+    explicit KGameClock(QObject *parent = 0, ClockType clocktype = HourMinSec);
+
+    ~KGameClock();
+
+    /**
+     * @return the total number of seconds elapsed.
+     */
+    uint seconds() const;
+
+    /**
+     * @return the time as a string to be displayed: "mm:ss".
+     */
+    QString minSecString() const;
+
+    /**
+     * @return the time as a string to be displayed: "hh:mm:ss".
+     */
+    QString hourMinSecString() const;
+
+    /**
+     * Set the time.
+     */
+    void setTime(uint seconds);
+
+    /**
+     * Set the time (format should be "hh:mm:ss").
+     */
+    void setTime(const QString &s);
+    
+    /**
+     * Refresh
+     */
+    void showTime();
+
+Q_SIGNALS:
+    void timeChanged(const QString &);
+
+public Q_SLOTS:
+    /**
+     * Reset the clock and start again from zero
+     */
+    virtual void restart();
+
+    /**
+     * Pause the clock
+     */
+    virtual void pause();
+
+    /**
+     * Resume counting time from the current position
+     */
+    virtual void resume();
+
+protected Q_SLOTS:
+    virtual void timeoutClock();
+
+private:
+    friend class KGameClockPrivate;
+    KGameClockPrivate *const d;
+
+    Q_DISABLE_COPY(KGameClock)
+};
+
+#endif
