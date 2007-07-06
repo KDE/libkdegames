ddb09520936fedc4e4017c835d51a50b22dea333
Nicolas Roffet <nicolas-kde@roffet.com>
2007-07-06 17:05:18 +0000

    I added a new class in libkdegames: "KGameDifficulty".
    In many games, the player can select and change the difficulty level. Until now, this is rewritten from scatch in every single game and it's not uniform. With KGameDifficulty, it should be easier and more consistant.
    Features:
     - The class uses the standard icon "games-difficult.png".
     - It's possible to use between 1 and 8 "standard" appelations for the levels (like "Easy", "Medium", "Hard", ...) or to customize them (and have more) if needed.
     - The class can ask the player to confirm, if he wants to start a new game to change the difficulty level, so that the game itself doesn't need to care much about this.
     - It's possible to defined a custom difficulty level (with a configuration dialog that has to be implemented in every game).
    
    The class is in a 1st draf state. It's working, but not yet full documented and wihout D-pointer yet. 
    We should also discuss if it's OK to select the difficulty level in the menu bar and / or in the toolbar. Or if we also want a independant widget for this, that could be placed in the status bar for instance, like in bovo right now.
    We should also include the menu item "Game difficulty" in the standard UI RC file of kdelibs. Before that, it has to be explicit added in the UI RC file of every game using this class.
    So, please give some feedback. :)
    
    (I spoke with Johann about this change at the aKademy.)
    
    I fixed also KBlackBox to use this class now. It's the first game to use it as a proof of concept. (And in KBlackBox it's better to have predefined difficulty levels, so the player doesn't have to configure the number of balls and the grid size like before).
    If the feedback is good, I would be glad to fix the other games.
    
    CCMAIL:kde-games-devel@kde.org
    
    svn path=/trunk/KDE/kdegames/libkdegames/; revision=684464

---
+++ kgamedifficulty.cpp
@@ -0,0 +1,124 @@
+/*
+Copyright (c) 2007, Nicolas Roffet, <nicolas-kde@roffet.com>
+
+This library is free software; you can redistribute it and/or modify it under the terms of the GNU Library General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+*/
+
+#include "kgamedifficulty.h"
+
+
+
+#include <kactioncollection.h>
+#include <kicon.h>
+#include <klocale.h>
+#include <kmessagebox.h>
+#include <kselectaction.h>
+#include <kxmlguiwindow.h>
+
+
+
+KGameDifficulty::KGameDifficulty(KXmlGuiWindow* window, const bool restartByChange, const int number)
+{
+	Q_ASSERT((number>0) && (number<9));
+
+	// Create level list
+	if (number>=7)
+		m_texts << i18n("Ridiculously easy");
+	if (number>=5)
+		m_texts << i18n("Very easy");
+	if (number>=3)
+		m_texts << i18n("Easy");
+	m_texts << i18n("Medium");
+	if (number>=2)
+		m_texts << i18n("Hard");
+	if (number>=4)
+		m_texts << i18n("Very hard");
+	if (number>=6)
+		m_texts << i18n("Almost impossible");
+	if (number>=8)
+		m_texts << i18n("Impossible");
+
+	createActionsAndMore(window, restartByChange);
+}
+
+
+KGameDifficulty::KGameDifficulty(KXmlGuiWindow* window, const bool restartByChange, QStringList& texts)
+{
+	Q_ASSERT(texts.count()>0);
+
+	m_texts = texts;
+
+	createActionsAndMore(window, restartByChange);
+}
+
+
+
+KGameDifficulty::~KGameDifficulty()
+{
+	delete m_menu;
+}
+
+
+void KGameDifficulty::addCustomLevel()
+{
+	QAction* separator = new QAction(m_menu);
+	separator->setSeparator(true);
+	m_menu->addAction(separator);
+
+	m_menu->addAction(i18n("Custom..."));
+}
+
+
+void KGameDifficulty::changeLevel(const int level)
+{
+	bool mayAbort = true;
+
+	// In the toolbar as a combobox, it's possible to select the separator!
+	if (m_menu->action(level)->isSeparator())
+		mayAbort = false;
+
+	if (mayAbort && m_restartByChange && m_running)
+		mayAbort = ( KMessageBox::warningContinueCancel(0, i18n("This will be the end of the current game!"), QString(), KGuiItem(i18n("Change the difficulty level"))) == KMessageBox::Continue );
+
+	if (mayAbort)
+		setLevel(level);
+	else
+		// restore current level selection
+		setLevel(m_level);
+}
+
+
+void KGameDifficulty::setLevel(const int level)
+{
+	m_menu->setCurrentItem(level);
+	if (level!=m_level)
+		emit levelChanged(level);
+	m_level = level;
+}
+
+
+void KGameDifficulty::setRunning(const bool running)
+{
+	m_running = running;
+}
+
+
+void KGameDifficulty::createActionsAndMore(KXmlGuiWindow* window, const bool restartByChange)
+{
+	m_restartByChange = restartByChange;
+	setParent(window);
+	setRunning(true);
+
+	m_menu = new KSelectAction(KIcon("games-difficult"), i18n("Game difficulty"), window);
+	window->actionCollection()->addAction("game_difficulty", m_menu);
+	connect(m_menu, SIGNAL(triggered(int)), this, SLOT(changeLevel(int)));
+	m_menu->setItems(m_texts);
+	m_menu->setToolTip(i18n("Set the difficulty level"));
+	m_menu->setWhatsThis(i18n("Set the difficulty level of the game."));
+}
+
+#include "kgamedifficulty.moc"
---
+++ kgamedifficulty.h
@@ -0,0 +1,106 @@
+/*
+Copyright (c) 2007, Nicolas Roffet, <nicolas-kde@roffet.com>
+
+This library is free software; you can redistribute it and/or modify it under the terms of the GNU Library General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+*/
+
+#ifndef KGAMEDIFFICULTY_H
+#define KGAMEDIFFICULTY_H
+
+
+
+#include <QtCore/QStringList>
+
+
+class KSelectAction;
+class KXmlGuiWindow;
+
+
+// Pourquoi?
+#include <libkdegames_export.h>
+
+
+
+//TODO: Use D-Pointer
+/**
+ * @brief KGameDifficuty
+ */
+class KDEGAMES_EXPORT KGameDifficulty : public QObject
+{
+	Q_OBJECT
+
+	public:
+		/**
+		 * @brief Constructor with standard difficuty levels
+		 *
+		 * @param window The window where to add menu items.
+		 * @param restartByChange Does the game have to be restarted, if the difficulty level changes? In this case, a confirmation dialog is displayed.
+		 * @param number Number of different difficulty levels
+		 */
+		KDEGAMES_EXPORT KGameDifficulty(KXmlGuiWindow* window, const bool restartByChange, const int number);
+
+		/**
+		 * @brief Constructor with custom difficulty levels
+		 *
+		 * @param window The window where to add menu items.
+		 * @param restartByChange Does the game have to be restarted, if the difficulty level changes? In this case, a confirmation dialog is displayed.
+		 * @param texts List of custumized level texts.
+		 */
+		KDEGAMES_EXPORT KGameDifficulty(KXmlGuiWindow* window, const bool restartByChange, QStringList& texts);
+
+
+		KDEGAMES_EXPORT ~KGameDifficulty();
+
+
+		/**
+		 * @brief A special item is added for custom difficulty level
+		 */
+		KDEGAMES_EXPORT void addCustomLevel();
+
+
+	Q_SIGNALS:
+		KDEGAMES_EXPORT void levelChanged(const int level);
+
+
+	public Q_SLOTS:
+		/**
+		 * @brief Player wants to change the difficulty level
+		 *
+		 * The difference with the mehtode "setLevel" is that the user may have to confirm that he agrees to end the current game (if needed).
+		 */
+		KDEGAMES_EXPORT void changeLevel(const int level);
+
+		/**
+		 * @brief Set the new current difficulty level
+		 */
+		KDEGAMES_EXPORT void setLevel(const int level);
+
+		/**
+		 * @brief Set the game state: Running or not
+		 * The game state should be defined if the value of m_restartByChange is true.
+		 */
+		KDEGAMES_EXPORT void setRunning(const bool running);
+
+
+	private:
+		void createActionsAndMore(KXmlGuiWindow* window, const bool restartByChange);
+
+
+		KSelectAction* m_menu;
+
+		/**
+		 * @brief Current difficulty level
+		 */
+		int m_level;
+
+		bool m_restartByChange;
+		bool m_running;
+
+		QStringList m_texts;
+};
+
+#endif //KGAMEDIFFICULTY_H
