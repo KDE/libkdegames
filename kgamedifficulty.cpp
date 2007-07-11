93834021a7ff5b0da2df1cc8e9b6559cdcf2faf2
Nicolas Roffet <nicolas-kde@roffet.com>
2007-07-11 22:34:10 +0000

    I rewrote the class KGameDifficulty to provide a better API. The class is now a singleton. I wrote also the APIDOX. I tried to consider the good feedback of Dmitry, thanks. Thanks Pino for your help also. Don't hesitate to continue to improve the class. :)
    As the API changed, I had to update KBlackBox.
    I removed also by the way 2 difficulty levels in KBlackBox, because I found there were too much levels.
    
    svn path=/trunk/KDE/kdegames/libkdegames/; revision=686659

--- kgamedifficulty.cpp
+++ kgamedifficulty.cpp
@@ -1,5 +1,6 @@
 /*
 Copyright (c) 2007, Nicolas Roffet, <nicolas-kde@roffet.com>
+Copyright (c) 2007, Pino Toscano, <toscano.pino@tiscali.it>
 
 This library is free software; you can redistribute it and/or modify it under the terms of the GNU Library General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 
@@ -10,6 +11,11 @@ You should have received a copy of the GNU Library General Public License along 
 
 #include "kgamedifficulty.h"
 
+
+
+#include <QMap>
+
+
 #include <kactioncollection.h>
 #include <kicon.h>
 #include <klocale.h>
@@ -17,146 +23,337 @@ You should have received a copy of the GNU Library General Public License along 
 #include <kselectaction.h>
 #include <kxmlguiwindow.h>
 
-class KGameDifficultyPrivate
+
+
+class KGameDifficultyPrivate : public QObject
 {
-public:
-	KGameDifficultyPrivate(KGameDifficulty* qq);
-	~KGameDifficultyPrivate();
+	Q_OBJECT
 
-	void createActionsAndMore(KXmlGuiWindow* window, bool restartByChange);
+	public:
+		KGameDifficultyPrivate(KXmlGuiWindow* window, const QObject* recvr, const char* slotStandard, const char* slotCustom);
+		~KGameDifficultyPrivate();
 
-	KGameDifficulty* q;
+		void rebuildActions();
+		void emitStandardLevelChanged(KGameDifficulty::standardLevel level);
+		void emitCustomLevelChanged(int key);
+	
+		/**
+		* @brief Current custom difficulty level
+		*/
+		int m_customLevel;
 
-	/**
-	 * @brief Current difficulty level
-	 */
-	int m_level;
-	KSelectAction* m_menu;
-	bool m_restartByChange;
-	bool m_running;
-	QStringList m_texts;
+		KGameDifficulty::standardLevel m_standardLevel;
+		QList<KGameDifficulty::standardLevel> m_standardLevels;
+		QMap<int, QString> m_customLevels;
+
+		KSelectAction* m_menu;
+		KGameDifficulty::onChange m_restartOnChange;
+		bool m_running;
+		int m_oldSelection;
+
+
+	public Q_SLOTS:
+		/**
+		 * @brief Player wants to change the difficulty level to a standard level
+		 *
+		 * The difference with the methode "setSelection" is that the player may have to confirm that he agrees to end the current game (if needed).
+		 * @param newSelection Selected item.
+		 */
+		void changeSelection(int newSelection);
+
+	Q_SIGNALS:
+		/**
+		 * @brief Current difficulty level changed to a standard level
+		 *
+		 * The game catchs this signal and restarts a game with the new standard difficulty level.
+		 * @param level New standard level.
+		 */
+		void standardLevelChanged(KGameDifficulty::standardLevel level);
+
+		/**
+		 * @brief Current difficulty level changed to a custom level
+		 *
+		 * The game catchs this signal and restarts a game with the new standard difficulty level.
+		 * @param key Custom level identifier.
+		 */
+		void customLevelChanged(int key);
+
+
+	private:
+		void setSelection(int newSelection);
 };
 
-KGameDifficultyPrivate::KGameDifficultyPrivate(KGameDifficulty* qq)
-    : q(qq)
+
+KGameDifficultyPrivate::KGameDifficultyPrivate(KXmlGuiWindow* window, const QObject* recvr, const char* slotStandard, const char* slotCustom = 0)
 {
+	Q_ASSERT(recvr!=0);
+
+	m_oldSelection = -1; // No valid selection
+	m_standardLevel = KGameDifficulty::noLevel;
+	m_restartOnChange = KGameDifficulty::restartOnChange;
+	m_running = false;
+
+	QObject::connect(this, SIGNAL(standardLevelChanged(KGameDifficulty::standardLevel)), recvr, slotStandard);
+	if (slotCustom!=0)
+		QObject::connect(this, SIGNAL(customLevelChanged(int)), recvr, slotCustom);
+
+	m_menu = new KSelectAction(KIcon("games-difficult"), i18n("Game difficulty"), window);
+	m_menu->setToolTip(i18n("Set the difficulty level"));
+	m_menu->setWhatsThis(i18n("Set the difficulty level of the game."));
+	QObject::connect(m_menu, SIGNAL(triggered(int)), this, SLOT(changeSelection(int)));
+	window->actionCollection()->addAction("game_difficulty", m_menu);
+
+	setParent(window);
 }
+
 
 KGameDifficultyPrivate::~KGameDifficultyPrivate()
 {
-	delete m_menu;
+	delete KGameDifficulty::self();
 }
 
-KGameDifficulty::KGameDifficulty(KXmlGuiWindow* window, bool restartByChange, int number)
-    : d(new KGameDifficultyPrivate(this))
+
+void KGameDifficultyPrivate::changeSelection(int newSelection)
 {
-	Q_ASSERT((number>0) && (number<9));
+	bool mayChange = true;
 
-	// Create level list
-	if (number>=7)
-		d->m_texts << i18n("Ridiculously easy");
-	if (number>=5)
-		d->m_texts << i18n("Very easy");
-	if (number>=3)
-		d->m_texts << i18n("Easy");
-	d->m_texts << i18n("Medium");
-	if (number>=2)
-		d->m_texts << i18n("Hard");
-	if (number>=4)
-		d->m_texts << i18n("Very hard");
-	if (number>=6)
-		d->m_texts << i18n("Almost impossible");
-	if (number>=8)
-		d->m_texts << i18n("Impossible");
+	// In the toolbar as a combobox, it's possible to select the separator!
+	// TODO: Check if this is a bug in KSelectAction
+	if (m_menu->action(newSelection)->isSeparator())
+		mayChange = false;
 
-	d->createActionsAndMore(window, restartByChange);
+	if (mayChange && (m_restartOnChange==KGameDifficulty::restartOnChange) && m_running)
+		mayChange = ( KMessageBox::warningContinueCancel(0, i18n("This will be the end of the current game!"), QString(), KGuiItem(i18n("Change the difficulty level"))) == KMessageBox::Continue );
+
+	if (mayChange) {
+		setSelection(newSelection);
+	}
+	else {
+		// restore current level selection
+		setSelection(m_oldSelection);
+	}
 }
 
 
-KGameDifficulty::KGameDifficulty(KXmlGuiWindow* window, bool restartByChange, QStringList& texts)
-    : d(new KGameDifficultyPrivate(this))
+void KGameDifficultyPrivate::rebuildActions()
 {
-	Q_ASSERT(texts.count()>0);
+	m_menu->clear();
+	qSort(m_standardLevels.begin(), m_standardLevels.end());
 
-	d->m_texts = texts;
+	QStringList texts;
+	foreach(KGameDifficulty::standardLevel level, m_standardLevels) {
+		switch (level) {
+			case KGameDifficulty::ridiculouslyEasy:
+				texts << i18n("Ridiculously easy");
+				break;
+			case KGameDifficulty::veryEasy:
+				texts << i18n("Very easy");
+				break;
+			case KGameDifficulty::easy:
+				texts << i18n("Easy");
+				break;
+			case KGameDifficulty::medium:
+				texts << i18n("Medium");
+				break;
+			case KGameDifficulty::hard:
+				texts << i18n("Hard");
+				break;
+			case KGameDifficulty::veryHard:
+				texts << i18n("Very hard");
+				break;
+			case KGameDifficulty::extremelyHard:
+				texts << i18n("Extremely hard");
+				break;
+			case KGameDifficulty::impossible:
+				texts << i18n("Impossible");
+				break;
+			case KGameDifficulty::custom:
+			case KGameDifficulty::configurable:
+			case KGameDifficulty::noLevel:
+				// Do nothing
+				break;
+		}
+	}
+	m_menu->setItems(texts);
 
-	d->createActionsAndMore(window, restartByChange);
+	if (m_customLevels.count()>0) {
+		foreach(QString s, m_customLevels)
+			m_menu->addAction(s);
+	}
+
+	if (m_standardLevels.contains(KGameDifficulty::configurable)) {
+		QAction* separator = new QAction(m_menu);
+		separator->setSeparator(true);
+		m_menu->addAction(separator);
+
+		m_menu->addAction(i18n("Custom..."));
+	}
+
+	// reselect the previous selected item.
+	if (m_standardLevel==KGameDifficulty::custom)
+		KGameDifficulty::setLevel(m_customLevel);
+	else if (m_standardLevels.contains(m_standardLevel))
+		KGameDifficulty::setLevel(m_standardLevel);
 }
+
+
+void KGameDifficultyPrivate::emitStandardLevelChanged(KGameDifficulty::standardLevel level)
+{
+	emit standardLevelChanged(level);
+}
+
+
+void KGameDifficultyPrivate::emitCustomLevelChanged(int key)
+{
+	emit customLevelChanged(key);
+}
+
+
+void KGameDifficultyPrivate::setSelection(int newSelection)
+{
+	int countWithoutConfigurable = m_standardLevels.count();
+	if (m_standardLevels.contains(KGameDifficulty::configurable))
+		countWithoutConfigurable--;
+
+	if ((m_standardLevels.contains(KGameDifficulty::configurable)) && (newSelection==m_menu->actions().count()-1))
+		KGameDifficulty::setLevel(KGameDifficulty::configurable);
+	else if(newSelection<countWithoutConfigurable)
+		KGameDifficulty::setLevel(m_standardLevels[newSelection]);
+	else
+		KGameDifficulty::setLevel((m_customLevels.uniqueKeys()).value(newSelection - countWithoutConfigurable));
+
+	m_oldSelection = newSelection;
+}
+
+
+//---//
+
+
+KGameDifficulty* KGameDifficulty::instance = 0;
 
 
 KGameDifficulty::~KGameDifficulty()
 {
-	delete d;
+	// We do not need to delete d, because d deletes us.
 }
 
 
-void KGameDifficulty::addCustomLevel()
+KGameDifficulty* KGameDifficulty::self()
 {
-	QAction* separator = new QAction(d->m_menu);
-	separator->setSeparator(true);
-	d->m_menu->addAction(separator);
-
-	d->m_menu->addAction(i18n("Custom..."));
+	if (instance==0)
+		instance = new KGameDifficulty();
+	return instance;
 }
 
 
-void KGameDifficulty::changeLevel(int level)
+void KGameDifficulty::init(KXmlGuiWindow* window, const QObject* recvr, const char* slotStandard, const char* slotCustom)
 {
-	bool mayAbort = true;
+	Q_ASSERT(self()->d==0);
 
-	// In the toolbar as a combobox, it's possible to select the separator!
-	if (d->m_menu->action(level)->isSeparator())
-		mayAbort = false;
+	self()->d = new KGameDifficultyPrivate(window, recvr, slotStandard, slotCustom);
+}
 
-	if (mayAbort && d->m_restartByChange && d->m_running)
-		mayAbort = ( KMessageBox::warningContinueCancel(0, i18n("This will be the end of the current game!"), QString(), KGuiItem(i18n("Change the difficulty level"))) == KMessageBox::Continue );
 
-	if (mayAbort)
-		setLevel(level);
-	else
-		// restore current level selection
-		setLevel(d->m_level);
+void KGameDifficulty::setRestartOnChange(onChange restart)
+{
+	Q_ASSERT(self()->d);
+
+	self()->d->m_restartOnChange = restart;
+}
+
+
+void KGameDifficulty::addStandardLevel(standardLevel level)
+{
+	Q_ASSERT(self()->d);
+
+	self()->d->m_standardLevels.append(level);
+	self()->d->rebuildActions();
+}
+
+
+void KGameDifficulty::removeStandardLevel(standardLevel level)
+{
+	Q_ASSERT(self()->d);
+
+	self()->d->m_standardLevels.removeAll(level);
+	self()->d->rebuildActions();
+}
+
+
+void KGameDifficulty::addCustomLevel(int key, const QString& appellation)
+{
+	Q_ASSERT(self()->d);
+
+	self()->d->m_customLevels.insert(key, appellation);
+	self()->d->rebuildActions();
+}
+
+
+void KGameDifficulty::removeCustomLevel(int key)
+{
+	Q_ASSERT(self()->d);
+
+	self()->d->m_customLevels.remove(key);
+	self()->d->rebuildActions();
 }
 
 
 void KGameDifficulty::setEnabled(bool enabled)
 {
-	Q_ASSERT(d->m_menu);
+	Q_ASSERT(self()->d->m_menu);
 
 	// TODO: Doing this never disable the combobox in the toolbar (just in the menu). It seems to be a bug in the class KSelectAction of kdelibs/kdeui/actions. To check and solve...
-	d->m_menu->setEnabled(enabled);
+	self()->d->m_menu->setEnabled(enabled);
 }
 
 
-void KGameDifficulty::setLevel(int level)
+void KGameDifficulty::setLevel(standardLevel level)
 {
-	d->m_menu->setCurrentItem(level);
-	if (level != d->m_level)
-		emit levelChanged(level);
-	d->m_level = level;
+	Q_ASSERT(self()->d);
+
+	if (level==configurable)
+		self()->d->m_menu->setCurrentItem(self()->d->m_menu->actions().count()-1);
+	else if (level!=custom)
+		self()->d->m_menu->setCurrentItem(self()->d->m_standardLevels.indexOf(level));
+
+	if (level != self()->d->m_standardLevel)
+		self()->d->emitStandardLevelChanged(level);
+	self()->d->m_standardLevel = level;
+
+	self()->d->m_oldSelection = self()->d->m_menu->currentItem();
+}
+
+
+void KGameDifficulty::setLevel(int key)
+{
+	Q_ASSERT(self()->d);
+
+	self()->d->m_standardLevel = custom;
+
+	int a = self()->d->m_standardLevels.count();
+	if (self()->d->m_standardLevels.contains(configurable))
+		a -= 1;
+	self()->d->m_menu->setCurrentItem((self()->d->m_customLevels.uniqueKeys()).indexOf(key) + a);
+
+	if (key != self()->d->m_customLevel)
+		self()->d->emitCustomLevelChanged(key);
+	self()->d->m_customLevel = key;
+
+	self()->d->m_oldSelection = self()->d->m_menu->currentItem();
 }
 
 
 void KGameDifficulty::setRunning(bool running)
 {
-	d->m_running = running;
+	Q_ASSERT(self()->d);
+
+	self()->d->m_running = running;
 }
 
 
-void KGameDifficultyPrivate::createActionsAndMore(KXmlGuiWindow* window, bool restartByChange)
+KGameDifficulty::KGameDifficulty()
 {
-	m_restartByChange = restartByChange;
-	q->setParent(window);
-	q->setRunning(true);
-
-	m_menu = new KSelectAction(KIcon("games-difficult"), i18n("Game difficulty"), window);
-	window->actionCollection()->addAction("game_difficulty", m_menu);
-	QObject::connect(m_menu, SIGNAL(triggered(int)), q, SLOT(changeLevel(int)));
-	m_menu->setItems(m_texts);
-	m_menu->setToolTip(i18n("Set the difficulty level"));
-	m_menu->setWhatsThis(i18n("Set the difficulty level of the game."));
-
-	q->setEnabled(true);
+	d = 0;
 }
 
 #include "kgamedifficulty.moc"
--- kgamedifficulty.h
+++ kgamedifficulty.h
@@ -1,5 +1,6 @@
 /*
 Copyright (c) 2007, Nicolas Roffet, <nicolas-kde@roffet.com>
+Copyright (c) 2007, Pino Toscano, <toscano.pino@tiscali.it>
 
 This library is free software; you can redistribute it and/or modify it under the terms of the GNU Library General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 
@@ -11,80 +12,164 @@ You should have received a copy of the GNU Library General Public License along 
 #ifndef KGAMEDIFFICULTY_H
 #define KGAMEDIFFICULTY_H
 
-#include <QtCore/QStringList>
 
-class KXmlGuiWindow;
 
-#include <libkdegames_export.h>
+class QObject;
+
 
 class KGameDifficultyPrivate;
+class KXmlGuiWindow;
+#include <libkdegames_export.h>
+
+
 
 /**
  * @brief KGameDifficuty
+ *
+ * KGameDifficulty manages the difficulty levels of a game. It provides standard actions on the main menu.
+ * This class is a singleton.
  */
-class KDEGAMES_EXPORT KGameDifficulty : public QObject
+class KDEGAMES_EXPORT KGameDifficulty
 {
-	Q_OBJECT
-
 	public:
 		/**
-		 * @brief Constructor with standard difficuty levels
+		 * @brief Behavior on change
 		 *
-		 * @param window The window where to add menu items.
-		 * @param restartByChange Does the game have to be restarted, if the difficulty level changes? In this case, a confirmation dialog is displayed.
-		 * @param number Number of different difficulty levels
+		 * Does the game have to be restarted, when the player changes the difficulty level? In this case and if the game is running, a confirmation dialog is displayed and the player can cancel the change.
 		 */
-		KGameDifficulty(KXmlGuiWindow* window, bool restartByChange, int number);
+		enum onChange {
+			restartOnChange, /** The current game has to be canceled and a new game will be started on change. */
+			noRestartOnChange /** The current game can continue on change. */
+		};
 
 		/**
-		 * @brief Constructor with custom difficulty levels
+		 * @brief Standard difficulty levels
 		 *
-		 * @param window The window where to add menu items.
-		 * @param restartByChange Does the game have to be restarted, if the difficulty level changes? In this case, a confirmation dialog is displayed.
-		 * @param texts List of custumized level texts.
+		 * If they fit the needs of the game, the standard appellations of the difficulty levels should be used.
 		 */
-		KGameDifficulty(KXmlGuiWindow* window, bool restartByChange, QStringList& texts);
+		enum standardLevel {
+			ridiculouslyEasy = 10, /** Level "Ridiculously easy" */
+			veryEasy = 20, /** Level "Very easy" */
+			easy = 30, /** Level "Easy" */
+			medium = 40, /** Level "Medium" */
+			hard = 50, /** Level "Hard" */
+			veryHard = 60, /** Level "Very hard" */
+			extremelyHard = 70, /** Level "Extremely hard" */
+			impossible = 80, /** Level "Impossible"  */
+			configurable = 90, /** Level "Custom...". This is a special item to let the player configure the difficulty level. The configuration of the user level has to be implemented in each game using it with an adapted dialog. Example: In a minesweeper game like KMines, the player wants to define the number of rows, columns and mines. */
+			custom = 100, /** Any custom appellations for levels */
+			noLevel = 110 /** No level */
+		};
 
+
+		/**
+		 * @brief Destructor
+		 */
 		virtual ~KGameDifficulty();
 
 		/**
-		 * @brief A special item is added for custom difficulty level
-		 */
-		void addCustomLevel();
-
-	Q_SIGNALS:
-		void levelChanged(int level);
-
-
-	public Q_SLOTS:
-		/**
-		 * @brief Player wants to change the difficulty level
+		 * @brief Access to the unique instance of the class
 		 *
-		 * The difference with the mehtode "setLevel" is that the user may have to confirm that he agrees to end the current game (if needed).
+		 * Be aware to call init first.
+		 * @see init
 		 */
-		void changeLevel(int level);
+		static KGameDifficulty* self();
+
+		/**
+		 * @brief Initialize the difficulty class
+		 *
+		 * You must call this class before using the singleton.
+		 *
+		 * @param window The window where to add menu items.
+		 * @param recvr Object that receives the signals and have the following slots.
+		 * @param slotStandard Slot to call when the player changed the difficulty level to a standard one.
+		 * @param slotCustom Slot to call when the player changed the difficulty level to a custom one. (Optional).
+		 */
+		static void init(KXmlGuiWindow* window, const QObject *recvr, const char* slotStandard, const char* slotCustom = 0);
+
+		/**
+		 * @brief Set if a new game has to be started by change
+		 *
+		 * Default is restartOnChange.
+		 * @param restart Behavior on change 
+		 */
+		static void setRestartOnChange(onChange restart);
+
+		/**
+		 * @brief Add a standard difficulty level
+		 *
+		 * You should add all the standard difficulty levels you need after the initialization of the class.
+		 * Standard difficulty levels are displayed before custom levels (if available).
+		 * @param levels Standard difficulty level to add
+		 */
+		static void addStandardLevel(standardLevel level);
+
+		/**
+		 * @brief Remove a standard difficulty level
+		 *
+		 * @param levels Standard difficulty level to remove
+		 */
+		static void removeStandardLevel(standardLevel level);
+
+		/**
+		 * @brief Add a custom difficulty level
+		 *
+		 * If you need custom appellations like "8x8", "Coward", "Courageous", "Tired of life" or whatever, you can define them with this method.
+		 * Custom difficulty levels are displayed after standard levels (if available).
+		 * @param key Custom level identifier. (It must be distinct for every different level. Trying to add a new level with an allocated key replace the previous level.).
+		 * @param appellation Custom level appellation.
+		 */
+		static void addCustomLevel(int key, const QString& appellation);
+
+		/**
+		 * @brief Remove a custom difficulty level
+		 *
+		 * @param key Custom level identifier.
+		 */
+		static void removeCustomLevel(int key);
 
 		/**
 		 * @brief Set if the difficulty level may be changed.
+		 *
 		 * If not, all the actions are disabled.
 		 * Default is "true".
+		 * @param enabled State.
 		 */
-		void setEnabled(bool enabled);
+		static void setEnabled(bool enabled);
 
 		/**
-		 * @brief Set the new current difficulty level
+		 * @brief Set the new current difficulty level as a standard one
+		 *
+		 * @param level Standard level.
 		 */
-		void setLevel(int level);
+		static void setLevel(standardLevel level);
+
+		/**
+		 * @brief Set the new current difficulty level as a custom one
+		 *
+		 * @param key Custom level identifier.
+		 */
+		static void setLevel(int key);
 
 		/**
 		 * @brief Set the game state: Running or not
-		 * The game state should be defined if the value of m_restartByChange is true.
+		 *
+		 * The game state should be defined if the current onChange value is restartByChange.
+		 * Default: false.
+		 * @param running Running state.
 		 */
-		void setRunning(bool running);
+		static void setRunning(bool running);
+
 
 	private:
+		/**
+		 * Private constructor: we are a singleton
+		 */
+		KGameDifficulty();
+
+		static KGameDifficulty* instance;
 		friend class KGameDifficultyPrivate;
-		KGameDifficultyPrivate* const d;
+		KGameDifficultyPrivate* d;
 
 		Q_DISABLE_COPY(KGameDifficulty)
 };
