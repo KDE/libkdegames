8a06423df2c3b07a75c7bd42970d50b3c97167bd
Andreas Pakulat <apaku@gmx.de>
2008-05-18 16:07:56 +0000

    Add my copyright to these files, thanks Albert for noticing. CCMAIL: aacid@kde.org
    svn path=/trunk/KDE/kdegames/libkdegames/; revision=809244

--- kcarddialog.h
+++ kcarddialog.h
@@ -1,5 +1,6 @@
 /*
     This file is part of the KDE games library
+    Copyright 2008 Andreas Pakulat <apaku@gmx.de>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
