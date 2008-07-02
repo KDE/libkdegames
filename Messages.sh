9bf1028871d23cd5c9351903cc9ed2c40a187366
Laurent Montel <montel@kde.org>
2008-07-02 06:49:42 +0000

    Remove temporary files
    svn path=/trunk/KDE/kdegames/libkdegames/; revision=827038

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 $EXTRACTRC $(find . -name "*.ui") >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.h -o -name \*.cpp -o -name \*.cc` -o $podir/libkdegames.pot 
+$XGETTEXT `find . -name \*.h -o -name \*.cpp -o -name \*.cc` -o $podir/libkdegames.pot
+rm -f rc.cpp 
