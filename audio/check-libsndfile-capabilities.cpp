16d5b69b9a81c692810745a36777fa991d953a4e
Stefan Majewsky <majewsky@gmx.net>
2012-02-15 18:49:58 +0000

    require Ogg/Vorbis support from libsndfile
    svn path=/trunk/KDE/kdegames/libkdegames/; revision=1280236

---
+++ audio/check-libsndfile-capabilities.cpp
@@ -0,0 +1,29 @@
+/***************************************************************************
+ *   Copyright 2012 Stefan Majewsky <majewsky@gmx.net>                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License          *
+ *   version 2 as published by the Free Software Foundation                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#include <sndfile.h>
+
+int main()
+{
+	//We need Ogg/Vorbis support. If sndfile.h is too old, these enum values
+	//will be missing and compiler errors will be generated.
+	(void) SF_FORMAT_OGG;
+	(void) SF_FORMAT_VORBIS;
+
+	return 0;
+}
