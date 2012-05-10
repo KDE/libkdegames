82a393d6b593888c64756416cafc7cfc6ca86c0a
Stefan Majewsky <majewsky@gmx.net>
2012-05-10 19:49:58 +0000

    Allow split build for all games except libkmahjongg dependencies.
    Most of this is straightforward. Some more complicated changes are in
    1. libkdegames: I needed to fix the headers <KgAudioScene> and
       <KgSound>, which did not refer to the right include location when
       installed.
    2. kgoldrunner: Its CMake code may not depend on OpenAL and libsndfile
       anymore. KGoldRunner therefore now uses the new
       <libkdegames_capabilities.h> header and enables sound only if
       KGAUDIO_BACKEND_OPENAL is set.
    
    KMahjongg and KShisen are not done yet. I could not create a
    config-script for KMahjongg because CMake currently gets confused over
    exported targets in multiple source directories. (D'oh...)
    
    svn path=/trunk/KDE/kdegames/libkdegames/; revision=1294007

---
+++ kgaudioscene.h
@@ -0,0 +1,3 @@
+//This file is needed because the <KgSound> header needs to link to
+//"../kgaudioscene.h" instead of "../audio/kgaudioscene.h".
+#include "audio/kgaudioscene.h"
---
+++ kgsound.h
@@ -0,0 +1,3 @@
+//This file is needed because the <KgSound> header needs to link to
+//"../kgsound.h" instead of "../audio/kgsound.h".
+#include "audio/kgsound.h"
