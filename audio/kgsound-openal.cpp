8d8da09f3aad8973c96e1264b2090d9a699fde70
Laszlo Papp <lpapp@kde.org>
2012-07-25 04:47:01 +0000

    Revert the sndlib.hh to sndlib.h change since high-level API is preferred
    svn path=/trunk/KDE/kdegames/libkdegames/; revision=1307641

--- audio/kgsound-openal.cpp
+++ audio/kgsound-openal.cpp
@@ -19,7 +19,7 @@
 #include "kgsound.h"
 #include "kgopenalruntime_p.h"
 
-#include <sndfile.h> //TODO: use Phonon instead of libsndfile for decoding
+#include <sndfile.hh> //TODO: use Phonon instead of libsndfile for decoding
 #include <KDE/KDebug>
 
 class KgSound::Private
