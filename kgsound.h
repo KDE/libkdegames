106ec8361efcfc90f74575a9cf539f1e373e2197
Frederik Schwarzer <schwarzer@kde.org>
2015-12-15 14:49:26 +0100

    Add license headers.

--- kgsound.h
+++ kgsound.h
@@ -1,3 +1,21 @@
+/***************************************************************************
+ *   Copyright 2010 Stefan Majewsky <majewsky@gmx.net>                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License          *
+ *   version 2 as published by the Free Software Foundation                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
 //This file is needed because the <KgSound> header needs to link to
 //"../kgsound.h" instead of "../audio/kgsound.h".
 #include "audio/kgsound.h"
