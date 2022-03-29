afd6857efd89119c894c7f4f323c2f97515959a0
Laurent Montel <montel@kde.org>
2022-03-29 13:20:38 +0200

    Adapt to new include (kf5.93)

--- src/kgamepopupitem.cpp
+++ src/kgamepopupitem.cpp
@@ -8,6 +8,10 @@
 
 // KF
 #include <KColorScheme>
+#include <kconfigwidgets_version.h>
+#if KCONFIGWIDGETS_VERSION >= QT_VERSION_CHECK(5, 93, 0)
+#include <KStatefulBrush> // was moved to own header in 5.93.0
+#endif
 // Qt
 #include <QPainter>
 #include <QTimeLine>
